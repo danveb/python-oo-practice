2a56db4637d4e7e47d0dbec86d2d93070bd505c7
Daniel Bae <danibae91@icloud.com>
2021-04-03 19:11:45 -0700

    work on code

--- serial.py
+++ serial.py
@@ -19,4 +19,29 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    # dunder init method (constructor); self and start=0 as param
+    def __init__(self, start=0):
+        """Create a starting number to count from and a counter to keep track of # times called"""
+        self.start = self.current_num = start 
+        self.count = -1 
+    
+    # instance method -> generate() 
+    def generate(self):
+        """return starting number first time called and increment # after"""
+        self.count += 1 
+        if self.count == 0:
+            return self.current_num 
+        else:
+            self.current_num += 1 
+            return self.current_num
+    
+    # instance method -> reset() 
+    def reset(self):
+        """reset number back to original start number"""
+        self.count = -1 
+        self.current_num = self.start 
 
+# instantiate 
+# serial = SerialGenerator(100)
+# serial.generate() 
+# serial.reset() 
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,42 @@
 """Word Finder: finds random words from a dictionary."""
-
+# file to read -> words.txt 
+# import random to use special methods
+import random 
 
 class WordFinder:
-    ...
+    """Finds random words from a dictionary"""
+
+    # dunder init method 
+    def __init__(self, file):
+        file_contents = open(file)
+        self.words = self.get_words(file_contents) 
+        print(f'{len(self.words)} words read')
+
+    # .get_words() method 
+    def get_words(self, file_contents):
+        """get words from file"""
+        return file_contents.read().splitlines() 
+
+    # .random() method 
+    def random(self):
+        """Returns a random word from a list of words""" 
+        return random.choice(self.words) 
+
+# Instantiate 
+# wf = WordFinder('words.txt') 
+# wf.random() 
+
+# subclass SpecialWordFinder
+class SpecialWordFinder(WordFinder):
+    """Method that returns a random word from a list of words but does not consider blank lines or words that are plain comments""" 
+
+    # dunder init method
+    def __init__(self, file):
+        super().__init__(file) 
+
+    def get_words(self, file_contents):
+        return [
+            word.strip()
+            for word in file_contents
+            if word.strip() and not word.startswith("#")
+        ]
